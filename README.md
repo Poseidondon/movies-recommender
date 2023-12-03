31a261293e17c977a1ce8d4b78e205b72fbb9853
Poseidondon <superzarub@rambler.ru>
2023-12-03 18:44:51 +0300

    README.md updated and small refactoring

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 ### Boris Zarubin
 ### b.zarubin@innopolis.university
 ### B21-DS-01
+# MovieLens recommendation system
+A recommender system is a type of information filtering system that suggests items or content
+to users based on their interests, preferences, or past behavior. These systems are commonly
+used in various domains, such as e-commerce, entertainment, social media, and online content
+platforms.
+
+## Installation
+Install [Tensorflow](https://www.tensorflow.org/install).
+```shell
+git clone https://github.com/Poseidondon/movies-recommender
+pip install -r requirements.txt
+```
+
+## Training
+*You can skip this part if you want to use pre-trained model.*
+
+Follow [2.0-training-and-visualization.ipynb](notebooks/2.0-training-and-visualization.ipynb) to
+train a model and save weights.
+
+## Prediction
+Python
+```python
+from src.movie_recommender import MovieRecommender
+
+model_path = 'models/best/DCN.ckpt'
+movie_rec = MovieRecommender(model_path)
+# here you select user for which you wish to receive a recommendation
+user_id = 345
+print(movie_rec.predict(user_id, recommend_seen=False))
+```
+
+## Evaluation
+TODO
--- src/movie_recommender.py
+++ src/movie_recommender.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import tensorflow_recommenders as tfrs
 import tensorflow_datasets as tfds
-tf.get_logger().setLevel('ERROR')
 import numpy as np
 
 from typing import Union
@@ -13,27 +12,34 @@ from DCN import MovielensModel
 class MovieRecommender:
     def __init__(self,
                  cp_path: Union[str, Path],
-                 ratings: tf.data.Dataset = None,
                  movies: tf.data.Dataset = None):
+        """
+        Loads dataset and model int memory.
+
+        :param cp_path: load model weights from that checkpoint
+        :param movies: dataset of all movies
+        """
+
         self.model = MovielensModel(0.5, [96, 32], [64, 32])
         self.model.compile(optimizer=tf.keras.optimizers.Adagrad(0.1))
         self.model.load_weights(cp_path)
 
-        if ratings is None:
-            ratings = tfds.load("movielens/100k-ratings", split="train")
-            ratings = ratings.map(lambda x: {
-                "movie_title": x["movie_title"],
-                "user_id": x["user_id"],
-                "user_rating": x["user_rating"],
-                "user_gender": int(x["user_gender"]),
-                "user_zip_code": x["user_zip_code"],
-                "user_occupation_text": x["user_occupation_text"],
-                "bucketized_user_age": int(x["bucketized_user_age"]),
-                "timestamp": x["timestamp"],
-            })
-            self.ratings = ratings.batch(4096).cache()
-        else:
-            self.ratings = ratings
+        ratings = tfds.load("movielens/100k-ratings", split="train")
+        ratings = ratings.map(lambda x: {
+            "movie_title": x["movie_title"],
+            "user_id": x["user_id"],
+            "user_rating": x["user_rating"],
+            "user_gender": int(x["user_gender"]),
+            "user_zip_code": x["user_zip_code"],
+            "user_occupation_text": x["user_occupation_text"],
+            "bucketized_user_age": int(x["bucketized_user_age"]),
+            "timestamp": x["timestamp"],
+        })
+        self.ratings = ratings.batch(4096).cache()
+        # train/test split is preserved due to random seed
+        tf.random.set_seed(42)
+        shuffled = ratings.shuffle(100_000, seed=42, reshuffle_each_iteration=False)
+        self.test = shuffled.skip(80_000).take(20_000).batch(4096).cache()
 
         if movies is None:
             self.movies = tfds.load("movielens/100k-movies", split="train")
@@ -48,9 +54,22 @@ class MovieRecommender:
         )
 
     def evaluate(self):
-        return self.model.evaluate(self.ratings, return_dict=True)
+        """
+        Return tensorflow metrics on validation dataset (20% partition of movielens-1ooK)
+
+        :return:
+        """
+
+        return self.model.evaluate(self.test, return_dict=True)
 
     def get_user_data(self, user_id):
+        """
+        Return user features used in DCN model in a convenient format
+
+        :param user_id: user_id in MovieLens
+        :return: tuple(user_data, movies_seen)
+        """
+
         user_id = str(user_id)
         user_ratings = self.ratings.unbatch().filter(lambda x: x['user_id'] == user_id)
         user_data = list(user_ratings.take(1))[0]
@@ -68,6 +87,15 @@ class MovieRecommender:
         return user_data, movies_seen
 
     def predict(self, user_id, recommend_seen=False, k=10):
+        """
+        Predict movies by user_id
+
+        :param user_id: user_id in MovieLens
+        :param recommend_seen: if True - recommend movies that user already seen
+        :param k: number of movies to recommend
+        :return: tuple(scores, movie_titles)
+        """
+
         user_data, movies_seen = self.get_user_data(user_id)
         if recommend_seen:
             scores, titles = self.index(user_data, k=k)
@@ -78,9 +106,11 @@ class MovieRecommender:
 
 
 if __name__ == "__main__":
+    # example of prediction
     model_dir = Path(__file__).parent.parent / 'models'
-    model_path = str(model_dir / 'DNN-7-96_32-64_32-emb_0.25-cross_emb' / 'best' / 'cp-174_loss-14645_acc100-0.3109_rmse-0.9815.ckpt')
+    model_path = str(model_dir / 'best' / 'DCN.ckpt')
     movie_rec = MovieRecommender(model_path)
+    print(movie_rec.evaluate())
     print(movie_rec.predict("13"))
     print(movie_rec.predict("15"))
     print(movie_rec.predict("345"))
